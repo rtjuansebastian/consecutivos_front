c243b15be912acfc00f69b75c0641bd8d55ee0da
Juan Rodriguez <rtjuansebastian@gmail.com>
2018-02-10 15:55:06 -0500

    Se agregó la tabla para presentar los usuarios en la vista de usuarios.

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -7,18 +7,67 @@ class Usuarios extends Component {
         super(props);
 
         this.state = {
+            usuarios: [{"cedula": "", "nombre": "", "correo": "", "equipo": {"id": 0, "nombre": "", "siglas": ""}, "iniciales": ""}]
         };
     }
 
+    componentWillMount() {
+        this.traerUsuarios();
+    }
 
+    traerUsuarios() {
+        fetch('http://localhost:8080/consecutivos/usuarios')
+                .then((response) => {
+                    return response.json();
+                })
+                .then((usuarios) => {
+                    this.setState({usuarios: usuarios});
+                });
+    }
+    
     render() {
 
         return (
                 <div className="row">                    
-
+                    <div className="col-md-6">
+                        <TablaUsuarios usuarios={this.state.usuarios} />
+                    </div>
                 </div>
                 );
     }
 }
+
+const TablaUsuarios = props => {
+    const FilasUsuarios = props.usuarios.map((usuario, index) => <FilaUsuario usuario={usuario} key={index}/>);
+    return(
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Cedula</th>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                        <th>Equipo</th>
+                        <th>Iniciales</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {FilasUsuarios}
+                </tbody>
+            </table>
+            );
+};
+
+const FilaUsuario = props => {
+
+    return (
+            <tr>
+                <td>{props.usuario.cedula}</td>
+                <td>{props.usuario.nombre}</td>
+                <td>{props.usuario.correo}</td>
+                <td>{props.usuario.equipo.nombre}</td>
+                <td>{props.usuario.iniciales}</td>
+            </tr>
+            );
+}; 
 
 export default Usuarios;
