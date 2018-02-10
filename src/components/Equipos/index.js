ef8d80e87af797943ecf10955f8de6e3bae01d9a
Juan Rodriguez <rtjuansebastian@gmail.com>
2018-02-10 15:31:12 -0500

    Se agregó la tabla para presentar los equipos en la vista de equipos.

--- src/components/Equipos/index.js
+++ src/components/Equipos/index.js
@@ -7,18 +7,61 @@ class Equipos extends Component {
         super(props);
 
         this.state = {
+            equipos: [{"id": 0, "nombre": "", "siglas": ""}]
         };
     }
 
+    componentWillMount() {
+        this.traerEquipos();
+    }
+
+    traerEquipos() {
+        fetch('http://localhost:8080/consecutivos/equipos')
+                .then((response) => {
+                    return response.json();
+                })
+                .then((equipos) => {
+                    this.setState({equipos: equipos});
+                });
+    }
 
     render() {
 
         return (
                 <div className="row">                    
-
+                    <div className="col-md-6">
+                        <TablaEquipos equipos={this.state.equipos} />
+                    </div>
                 </div>
                 );
     }
 }
+
+const TablaEquipos = props => {
+    const FilasEquipos = props.equipos.map((equipo, index) => <FilaEquipo equipo={equipo} key={index}/>);
+    return(
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Siglas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {FilasEquipos}
+                </tbody>
+            </table>
+            );
+};
+
+const FilaEquipo = props => {
+
+    return (
+            <tr>
+                <td>{props.equipo.nombre}</td>
+                <td>{props.equipo.siglas}</td>
+            </tr>
+            );
+};
 
 export default Equipos;
