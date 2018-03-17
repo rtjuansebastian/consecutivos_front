ed9671ddcb551e9ba0fe0cd7f1290e486931a3fe
Juan Rodriguez <rtjuansebastian@gmail.com>
2018-03-16 21:33:23 -0500

    Primer versión de login para el generador de consecutivos.

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,93 @@
 //Dependencies
-import React, { Component }
-from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Navbar, Button } from 'react-bootstrap';
 
 //Components
-import Header from './Global/Header'
-import Content from './Global/Content'
+import Header from './Global/Header';
+import Content from './Global/Content';
 
 class App extends Component {
 
-static propTypes = {
-children: PropTypes.object.isRequired
-};
-        render() {
-
-const {children} = this.props;
+    static propTypes = {
+        children: PropTypes.object.isRequired
+    };
+    
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);        
+        this.state = {
+            auth:{}
+        };
         
+        this.props.auth.handleAuthentication();
+    };
+    
+    
+    
+    componentWillMount() {
+        //this.setState({auth:this.props.auth});
+        //this.setState({auth:this.props.auth},()=>{this.props.auth.handleAuthentication()}); 
+        //this.props.auth.handleAuthentication();
+
+    }    
+    
+    componentDidMount() {                
+        //this.state.auth.handleAuthentication();        
+    }
+    
+    goTo(route) {
+        this.props.history.replace(`/${route}`)
+    }
+
+    login() {
+        this.props.auth.login();        
+    }
+
+    logout() {
+        this.props.auth.logout();
+    }    
+        
+    render() {        
+        const {children} = this.props;        
         return (
-            <div className="App">
-                <Header/>
-                <Content body={children} />
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                <footer className="App-footer">
-                    Brecha Digital 2018
-                </footer>
-            </div>
+                <div className="App">
+                    <Header/>
+                    {
+                        this.props.auth.isAuthenticated() && (
+                                    <div>                                                                 
+                                        <Content body={children} />
+                                    </div>
+                                )
+                    }
+                    {
+                        !this.props.auth.isAuthenticated() && (
+                                    <div className="col-md-4">
+                                        <form onSubmit={this.login.bind(this)}>
+                                            <div className="form-group">
+                                                <label>Contraseña</label>
+                                                <input className="form-control" id="login-password" type="password" />
+                                            </div>
+                                            <Button
+                                                bsStyle="primary"
+                                                className="btn-margin"
+                                                onClick={this.login.bind(this)}
+                                                >
+                                                Ingresar
+                                            </Button>
+                                        </form>
+                                    </div>
+                                )
+                    }                        
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <footer className="App-footer">
+                        Brecha Digital 2018
+                    </footer>
+                </div>
                 );
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,19 @@
 //Dependencies
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
+import Auth from './Auth/Auth';
 
 //Components
 import App from './components/App';
 import Documentos from './components/Documentos';
 import Solicitudes from './components/Solicitudes';
 
+const auth = new Auth();
 const AppRoutes = () =>
-    <App>
-        <Switch>
-            <Route exact path="/" component={Documentos} />            
+    <App auth={auth}>
+        <Switch>                    
+            <Route exact path="/" />            
+            <Route path="/documentos" component={Documentos} />            
             <Route path="/solicitudes" component={Solicitudes} />            
         </Switch>
     </App>;
